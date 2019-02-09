4b2a543d0c18473069caba8695a7eb91c575b4cb
jonas@valfridsson.net <jonas@valfridsson.net>
2019-02-09 22:06:49 +0100

    Simone broke the regex, lame

--- test/random_package_random_drop_test.py
+++ test/random_package_random_drop_test.py
@@ -1,3 +1,3 @@
 import baselines_random.random_package_random_drop as rprd
 print("Starting evaluation")
-rprd.evaluate(render=False, robots=5, steps=1000)
+rprd.evaluate(render=True, robots=5, shelve_width=10, shelve_height=10, steps=1000)
