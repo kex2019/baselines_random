3b8f54a75e9a4b4e430f4427af6f70da63aaa0dc
jonas@valfridsson.net <jonas@valfridsson.net>
2019-02-09 22:02:53 +0100

    functioning baseline

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="BaselinesRandom",
+    version="0.0.1",
+    author="Jonas & Johan",
+    author_email="jonas@valfridsson.net",
+    description="Utils for warehouse simulator",
+    url="https://github.com/kex2019/Utilities",
+    packages=["baselines_random"],
+    install_requires=["numpy==1.14.2"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
---
+++ test/random_package_random_drop_test.py
@@ -0,0 +1,3 @@
+import baselines_random.random_package_random_drop as rprd
+print("Starting evaluation")
+rprd.evaluate(render=False, robots=5, steps=1000)
