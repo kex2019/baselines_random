10e18ce60bde7ad0ab0e021dd796d98bbfe54944
jonas@valfridsson.net <jonas@valfridsson.net>
2019-02-14 16:29:47 +0100

    this is Spartaaaaaaaa

--- test/random_package_random_drop_test.py
+++ test/random_package_random_drop_test.py
@@ -1,3 +1,3 @@
 import baselines_random.random_package_random_drop as rprd
 print("Starting evaluation")
-rprd.evaluate(render=True, robots=5, shelve_width=10, shelve_height=10, steps=1000)
+rprd.evaluate(render=True, robots=20, spawn=100, shelve_length=5, shelve_width=5, shelve_height=5, steps=1000)
