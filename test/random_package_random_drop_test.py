7db16546a336806dc25319c213dccd3e271c0373
jonas@valfridsson.net <jonas@valfridsson.net>
2019-02-26 13:08:04 +0100

    debugo

--- test/random_package_random_drop_test.py
+++ test/random_package_random_drop_test.py
@@ -1,5 +1,4 @@
 import baselines_random.random_package_random_drop as rprd
 print("Starting evaluation")
-rprd.evaluate(render=False, robots=20, spawn=100, shelve_length=5, shelve_width=5, shelve_height=5, steps=1000)
-
+rprd.evaluate(render=True, robots=20, spawn=100, shelve_length=5, shelve_width=5, shelve_height=5, steps=1000, collect=True)
 
