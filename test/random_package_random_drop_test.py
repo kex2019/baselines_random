8f20d7fa4207266d3fd8628cf996a9b861005deb
jonas@valfridsson.net <jonas@valfridsson.net>
2019-03-05 19:53:50 +0100

    This is why git rebase is a horrible horrible thing.

--- test/random_package_random_drop_test.py
+++ test/random_package_random_drop_test.py
@@ -1,4 +1,4 @@
 import baselines_random.random_package_random_drop as rprd
 print("Starting evaluation")
-rprd.evaluate(render=True, robots=2, spawn=20, capacity=3, shelve_length=5, shelve_width=2, shelve_height=2, steps=1000, periodicity_lower=200, periodicity_upper=700, collect=True)
+rprd.evaluate(render=True, robots=1, spawn=20, capacity=3, shelve_length=5, shelve_width=2, shelve_height=2, steps=1000, periodicity_lower=200, periodicity_upper=700, collect=True)
 
